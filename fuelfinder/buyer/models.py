af847c77c830092e363df5cbdb10281fdb001ce9
Timothy Ngorima <timothytakudzwa@gmail.com>
2019-11-23 14:55:45 +0200

    Adding fuel request

--- fuelfinder/buyer/models.py
+++ fuelfinder/buyer/models.py
@@ -1,10 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
-from supplier.models import FuelRequest
+
 
 # Create your models here.
  
+
+class FuelRequest(models.Model):
+    name = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    split = models.BooleanField(default=False)
+    fuel_type = models.CharField(max_length=20)
+    payment_method = models.CharField(max_length=200)
+    delivery_method = models.CharField(max_length=200)
+    date = models.DateField(auto_now_add=True)
+    time = models.TimeField(auto_now_add=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['date', 'time', 'name']
+
+    def __str__(self):
+        return f'{str(self.name)} - {str(self.amount)}'
+
 class BuyerProfile(models.Model):
     name = models.OneToOneField(User, on_delete=models.CASCADE, related_name='buyer_name')
     fuel_request = models.OneToOneField(FuelRequest, on_delete=models.CASCADE, related_name='fuel')
