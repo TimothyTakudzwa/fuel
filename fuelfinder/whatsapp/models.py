6b553f8a391dd943df6163e2df5e27b44cc2463b
Timothy Ngorima <timothytakudzwa@gmail.com>
2019-11-23 14:54:57 +0200

    Adding fuel request

--- fuelfinder/whatsapp/models.py
+++ fuelfinder/whatsapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class FuelRequest(models.Model):
+    name = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    split = models.BooleanField(default=False)
+    fuel_type = models.CharField(max_length=20)
+    payment_method = models.CharField(max_length=200)
+    delivery_method = models.CharField(max_length=200)
+    date = models.DateField(auto_now_add=True)
+    time = models.TimeField(auto_now_add=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['date', 'time', 'name']
+
+    def __str__(self):
+        return f'{str(self.name)} - {str(self.amount)}'
