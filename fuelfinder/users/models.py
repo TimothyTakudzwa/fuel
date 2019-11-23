a2c31cd3637057ee40300887d3b1e5933234eeec
Kallou0 <nzenzamk@gmail.com>
2019-11-23 03:48:30 -0800

    new repo

--- fuelfinder/users/models.py
+++ fuelfinder/users/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.dispatch import receiver
 
-# Create your models here.
+from supplier.models import *
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+class SupplierContact(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    supplier_profile = models.ForeignKey(SupplierProfile,on_delete=models.CASCADE)
+    telephone = models.CharField(max_length=300, blank=True)
+    cellphone = models.CharField(max_length=300, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['user__last_name']
+
+    def __str__(self):
+        return self.cellphone
+
+    def disable(self):
+        self.user.active = False
+        self.user.save()
+        self.active = False
+        self.save()
+
+    def enable(self):
+        self.user.active = True
+        self.user.save()
+        self.active = True
+        self.save()
+
+
